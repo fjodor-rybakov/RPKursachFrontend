dd0ce7754a5050ac48909cd61c2d25dcf3a3119f
fjodor-rybakov <fedryb@mail.ru>
2019-05-12 22:47:34 +0300

    update requests

--- src/services/transport/Transport.ts
+++ src/services/transport/Transport.ts
@@ -18,90 +18,95 @@ import {IProductListResponse} from "./interfaces/catalog/IProductListResponse";
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 
 export class Transport {
-    private readonly client: AxiosInstance;
+    private readonly backendApiClient: AxiosInstance;
+    private readonly showcaseApiClient: AxiosInstance;
 
     constructor() {
-        const option: AxiosRequestConfig = {
-            baseURL: config.server.url
+        let option: AxiosRequestConfig = {
+            baseURL: config.server.backendApiUrl
         };
-        this.client = axios.create(option);
+        this.backendApiClient = axios.create(option);
+        option = {
+            baseURL: config.server.showcaseApiUrl
+        };
+        this.showcaseApiClient = axios.create(option);
     }
 
     // user
 
     public async registration(params: IRegistrationParams): Promise<AxiosResponse<IMessage>> {
-        return this.client.post(EApiRoutes.REGISTRATION, params);
+        return this.backendApiClient.post(EApiRoutes.REGISTRATION, params);
     }
 
     public async login(params: ILoginParams): Promise<AxiosResponse<IToken>> {
-        return this.client.post(EApiRoutes.LOGIN, params);
+        return this.backendApiClient.post(EApiRoutes.LOGIN, params);
     }
 
     public async getUserInfo(): Promise<AxiosResponse<IUser>> {
-        return this.client.get(EApiRoutes.USER_INFO, Transport.getHeaderToken());
+        return this.backendApiClient.get(EApiRoutes.USER_INFO, Transport.getHeaderToken());
     }
 
     public async getPurchaseHistory(): Promise<AxiosResponse<Omit<IPurchaseHistory, "ProductId" & "UserId" & "Id">>> {
-        return this.client.get(EApiRoutes.USER_PURCHASE_HISTORY, Transport.getHeaderToken());
+        return this.backendApiClient.get(EApiRoutes.USER_PURCHASE_HISTORY, Transport.getHeaderToken());
     }
 
     public async getAllPurchaseHistory(): Promise<AxiosResponse<IPurchaseHistory[]>> {
-        return this.client.get(EApiRoutes.PURCHASE_HISTORY_ALL, Transport.getHeaderToken());
+        return this.backendApiClient.get(EApiRoutes.PURCHASE_HISTORY_ALL, Transport.getHeaderToken());
     }
 
     public async cancelOrder(orderId: number): Promise<AxiosResponse<IMessage>> {
-        return this.client.delete(EApiRoutes.PURCHASE_HISTORY.replace(":id", orderId.toString()), Transport.getHeaderToken());
+        return this.backendApiClient.delete(EApiRoutes.PURCHASE_HISTORY.replace(":id", orderId.toString()), Transport.getHeaderToken());
     }
 
     // catalog
 
     public async getCompaniesList(): Promise<AxiosResponse<ICompany[]>> {
-        return this.client.get(EApiRoutes.COMPANIES_LIST);
+        return this.showcaseApiClient.get(EApiRoutes.COMPANIES_LIST);
     }
 
     public async getCategoriesList(): Promise<AxiosResponse<ICategory[]>> {
-        return this.client.get(EApiRoutes.CATEGORIES_LIST);
+        return this.showcaseApiClient.get(EApiRoutes.CATEGORIES_LIST);
     }
 
     public async getProductList(page: number, limit: number, filter?: string): Promise<AxiosResponse<IProductListResponse>> {
         const options = {params: {page, limit, filter}};
-        return this.client.get(EApiRoutes.PRODUCTS, options);
+        return this.showcaseApiClient.get(EApiRoutes.PRODUCTS, options);
     }
 
     public async addNewProduct(params: IProductParams): Promise<AxiosResponse<IMessage>> {
-        return this.client.post(EApiRoutes.PRODUCTS, params, Transport.getHeaderToken());
+        return this.showcaseApiClient.post(EApiRoutes.PRODUCTS, params, Transport.getHeaderToken());
     }
 
     public async updateProduct(params: Partial<IProductParams>, productId: number): Promise<AxiosResponse<IMessage>> {
-        return this.client.put(EApiRoutes.PRODUCT.replace(":id", productId.toString()), params, Transport.getHeaderToken());
+        return this.showcaseApiClient.put(EApiRoutes.PRODUCT.replace(":id", productId.toString()), params, Transport.getHeaderToken());
     }
 
     public async deleteProduct(productId: number): Promise<AxiosResponse<IMessage>> {
-        return this.client.delete(EApiRoutes.PRODUCT.replace(":id", productId.toString()), Transport.getHeaderToken());
+        return this.showcaseApiClient.delete(EApiRoutes.PRODUCT.replace(":id", productId.toString()), Transport.getHeaderToken());
     }
 
     public async uploadProductImage(file: File, productId: number): Promise<AxiosResponse<IMessage>> {
         let formData = new FormData();
         formData.append('file', file);
-        return this.client.post(EApiRoutes.UPLOAD_PRODUCT_IMAGE.replace(":id", productId.toString()), formData, Transport.getHeaderToken());
+        return this.showcaseApiClient.post(EApiRoutes.UPLOAD_PRODUCT_IMAGE.replace(":id", productId.toString()), formData, Transport.getHeaderToken());
     }
 
     // basket
 
     public async addBasketProduct(params: BasketProductParams): Promise<AxiosResponse<IMessage>> {
-        return this.client.post(EApiRoutes.BASKET_PRODUCTS, params, Transport.getHeaderToken());
+        return this.backendApiClient.post(EApiRoutes.BASKET_PRODUCTS, params, Transport.getHeaderToken());
     }
 
     public async getBasketProductList(): Promise<AxiosResponse<IProduct[]>> {
-        return this.client.get(EApiRoutes.BASKET_PRODUCTS, Transport.getHeaderToken());
+        return this.backendApiClient.get(EApiRoutes.BASKET_PRODUCTS, Transport.getHeaderToken());
     }
 
     public async deleteBasketProduct(productId: number): Promise<AxiosResponse<IMessage[]>> {
-        return this.client.delete(EApiRoutes.BASKET.replace(":id", productId.toString()), Transport.getHeaderToken());
+        return this.backendApiClient.delete(EApiRoutes.BASKET.replace(":id", productId.toString()), Transport.getHeaderToken());
     }
 
     public async buyBasketProducts(params: BasketProductParams[]): Promise<AxiosResponse<IMessage[]>> {
-        return this.client.post(EApiRoutes.BUY_PRODUCTS, params, Transport.getHeaderToken());
+        return this.backendApiClient.post(EApiRoutes.BUY_PRODUCTS, params, Transport.getHeaderToken());
     }
 
     private static getHeaderToken() {
